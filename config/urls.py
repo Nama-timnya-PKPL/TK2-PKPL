9ad4aae045ad4054cf7409eabd25c70c60bfa082
aliakb-12 <alimurtadha2006@gmail.com>
2026-04-02 00:23:35 +0700

    resolve conflicts

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
 
 # media (gambar)
